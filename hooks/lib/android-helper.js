16c0e81189f89e9171aa91b133bec30a99e8cdcf
boedy <boudewijng@gmail.com>
2019-07-05 13:12:18 +0200

    support for lambda versions in older cordova versions

---
+++ hooks/lib/android-helper.js
@@ -0,0 +1,39 @@
+
+var fs = require("fs");
+var path = require("path");
+var utilities = require("./utilities");
+
+module.exports = {
+
+    addFabricBuildToolsGradle: function() {
+
+        var buildGradle = utilities.readBuildGradle();
+
+        buildGradle +=  [
+            "",
+            "// Cordova-background-mode-plugin - Start ",
+            "buildscript {",
+            "    repositories {",
+            "        mavenCentral()",
+            "    }",
+            "    dependencies {",
+            "        classpath 'me.tatarka:gradle-retrolambda:3.2.4'",
+            "    }",
+            "}",
+            "",
+            "apply plugin: 'me.tatarka.retrolambda'",
+            "// Cordova-background-mode-plugin - End",
+        ].join("\n");
+
+        utilities.writeBuildGradle(buildGradle);
+    },
+
+    removeFabricBuildToolsFromGradle: function() {
+
+        var buildGradle = utilities.readBuildGradle();
+
+        buildGradle = buildGradle.replace(/\n\/\/ Cordova-background-mode-plugin - Start[\s\S]*\/\/ Cordova-background-mode-plugin - End/, "");
+
+        utilities.writeBuildGradle(buildGradle);
+    }
+};
